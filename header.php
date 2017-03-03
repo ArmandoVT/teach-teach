34474ebe531ddab1df719e0d82d1d93a6b2430bd
Armando_Vizcaino <rudolf.crazy@gmail.com>
2017-03-03 16:17:31 -0600

    Creando repositorio

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Teach & Teach</title>
+		<link rel="stylesheet" type="text/css" href="css/estilos.css">
+		<style type="text/css">
+			@font-face {
+				font-family: AvenirNextLTPro-Regular;
+				src: url(tipografia/avenir-next-lt-pro/AvenirNextLTPro-Regular.otf);
+			}
+		</style>
+	</head>
+		<body>
+			<div class="menu">
+				<img class="menu-img" src="imagenes/iconos-02.png">
+				<div class="menu-texto">
+					<h1 class="menu-titulo">Teach&Teach</h1>
+					<input class="menu-input" type="text" placeholder="  Buscar en Teach&Teach">
+					<a class="menu-link-1" href="">Log in</a>
+					<div class="menu-border"></div> 
+					<a class="menu-link-2" href="">Regístrate</a>
+				</div>
+			</div>
