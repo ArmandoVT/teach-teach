db7966302f4b8b23de2faaba11520bfc6f4536e9
Victor <vincinz@gmail.com>
2017-03-06 19:05:35 -0600

    correciones

---
+++ body_cuatro.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+
+    
+
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body class="body_cuatro">
+  <div class="body_cuatro_contenido">
+    <form>
+        <p>Titulo de la asesoría:</p>
+        <input class="titulo_asesoria" type="text" name="firstname">
+        <br>
+        <p>Describe la asesoría:</p>
+        <textarea class="textarea" cols="10" rows="5"></textarea>
+        <p>Categoría:</p>
+        <select class="categoria">
+          <option value=""></option>
+          <option value=""></option>
+          <option value=""></option>
+          <option value=""></option>
+        </select>
+        <p>Fecha y Hora de entrega:</p>
+        <textarea class="fecha_hora" cols="20" rows="12"></textarea>
+    </form>
+  </div>
+
+
+</body>
+
+</html>
---
+++ body_tres.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    
+
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body class="body_tres">
+    <div class="colmask leftmenu">
+        <div class="colleft">
+
+            <div class="col1">
+                <div class="derecha">
+                    <table>
+                        <tr>
+                            <th>Asesoria</th>
+                            <th>Fecha de entrega</th>
+                            <th>Categoria</th>
+                            <th>Cotizaciones</th>
+                            <th>Cotizar</th>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+
+                    </table>
+                </div>
+
+            </div>
+            <div class="col2">
+                <div class="">
+                    <h2 class="cat">Categorías</h2>
+                </div>
+                <div class="izquierda">
+                    <a href="#">
+                        <img src="imagenes/iconos-03.png" class="image">
+                        <p>Física/<br>Matemáticas</p>
+                    </a>
+                </div>
+                <div class="izquierda_2">
+                    <a href="#">
+                        <img src="imagenes/iconos-04.png" class="image">
+                        <p>Médico/<br>Biológicas</p>
+                    </a>
+                </div>
+                <div class="izquierda_2">
+                    <a href="#">
+                        <img src="imagenes/iconos-05.png" class="image">
+                        <p>Humanidades/<br>Sociales</p>
+                    </a>
+                </div>
+                <div class="izquierda_2">
+                    <a href="#">
+                        <img src="imagenes/iconos-06.png" class="image">
+                        <p>Artes/<br>Diseño</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
